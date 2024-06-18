ca9c187968d64ecdf9eaa4cc51e9ac37bd4bd456
jungsikjeong <wndtlr1024@gmail.com>
2024-06-18 13:06:28 +0900

    sitemap 제거

--- next-sitemmap.config.js
+++
@@ -1,8 +0,0 @@
-import { CONFIG } from './site.config';
-
-module.exports = {
-  siteUrl: `https://${CONFIG.domain}`,
-  generateRobotsTxt: true,
-  sitemapSize: 7000,
-  generateIndexSitemap: false,
-};
